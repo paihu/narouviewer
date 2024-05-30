1532a61cb24a703a407937990e3f3772fa6a7e61
paihu <13479783+paihu@users.noreply.github.com>
2024-05-30 20:16:19 +0900

    add novel page link

--- app/src/main/java/dev/paihu/narou_viewer/ui/NovelScreen.kt
+++ app/src/main/java/dev/paihu/narou_viewer/ui/NovelScreen.kt
@@ -1,5 +1,7 @@
 package dev.paihu.narou_viewer.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -22,11 +24,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -92,6 +96,7 @@ fun NovelCard(
     delete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     var expanded by remember { mutableStateOf(false) }
     Card(modifier = modifier) {
         Row(modifier = Modifier.fillMaxWidth()) {
@@ -138,6 +143,19 @@ fun NovelCard(
                             download()
                         },
                         label = { Text("更新") },
+                        selected = false,
+                    )
+                    NavigationDrawerItem(
+                        onClick = {
+                            expanded = false
+                            val uri = when (novel.type) {
+                                "narou" -> "https://ncode.syosetu.com/" + novel.novelId
+                                else -> "https://kakuyomu.jp/works/" + novel.novelId
+                            }
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                            startActivity(context, intent, null)
+                        },
+                        label = { Text("小説ページ") },
                         selected = false,
                     )
                 }
